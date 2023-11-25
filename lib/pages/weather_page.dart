a6a230401e4145a4229f36fb6489b3bf226b25a5
Brian <barimaagyemang2011@gmail.com>
2023-11-25 12:50:24 +0000

    Initial commit

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  // api key
+  final _weatherService = WeatherService('a3bea06ca57746ecbec3f51f2d1008b6');
+  Weather? _weather;
+
+  // fetch weather
+  _fetchWeather() async {
+    // get the current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    // get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+
+    // any errors
+    catch (e) {
+      print(e);
+    }
+  }
+
+  // weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'Assests/sun.json'; // default to sunny
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'Assests/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'Assests/rainy.json';
+      case 'thunderstorm':
+        return 'Assests/thunder.json';
+      case 'clear':
+        return 'Assests/sun.json';
+      default:
+        return 'Assests/sun.json';
+    }
+  }
+
+  // init state
+  @override
+  void initState() {
+    super.initState();
+
+    // fetch weather on startup
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // city name
+            Text(_weather?.cityName ?? "loading city.."),
+
+            // animation
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+            // temperature
+            Text('${_weather?.temperature.round()}°C'),
+
+            // weather condition
+            Text(_weather?.mainCondition ?? "")
+          ],
+        ),
+      ),
+    );
+  }
+}
